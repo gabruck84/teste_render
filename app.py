820575847d8b5ee73c913bebe204e88f67d8b0b0
gabruck84 <89227166+gabruck84@users.noreply.github.com>
2025-03-16 00:52:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def test_selenium():
     # Configuração do WebDriver
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    options.add_argument("--no-sandbox") 
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")  # Desabilitar GPU (para evitar erros no modo headless)
     options.add_argument("--incognito")# Rodar sem abrir a janela do navegador
 
     # Iniciar o WebDriver com o ChromeDriver gerenciado pelo webdriver-manager
