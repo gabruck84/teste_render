2c4c21ccef9dbe86b5bfbc692762a6d4b11c43bf
gabruck84 <89227166+gabruck84@users.noreply.github.com>
2025-03-16 00:39:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def test_selenium():
     # Configuração do WebDriver
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox") 
     options.add_argument("--incognito")# Rodar sem abrir a janela do navegador
 
     # Iniciar o WebDriver com o ChromeDriver gerenciado pelo webdriver-manager
